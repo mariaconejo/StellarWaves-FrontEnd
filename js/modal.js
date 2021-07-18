153d14a35a987f73faf6ce804f745078988a2bbc
Jeareth Venegas <venegasjea@gmail.com>
2021-07-17 19:17:55 -0600

    estilos corregidos del modal

--- js/modal.js
+++ js/modal.js
@@ -12,12 +12,10 @@ btn__addSong.onclick = function() {
     modal__Songs.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
 span.onclick = function() {
     modal__Songs.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
         modal__Songs.style.display = "none";
