31df187ff45ecd3c3a048922a12a9cbe7a2f47c0
mariaconejo <mary03081@gmail.com>
2021-07-18 18:52:08 -0600

    se agrego cambios

--- js/canvas.js
+++ js/canvas.js
@@ -1,91 +1,121 @@
-const container = document.getElementById('container');
-const canvas = document.getElementById('canvas1');
-const file = document.getElementById('fileupload');
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-const ctx = canvas.getContext('2d');
-//ctx.globalCompositeOperation = 'difference'
-ctx.lineCap = 'round';
-let audioSource;
-let analyser;
+function App() {
+	var audioElement = document.getElementById('audioElement');
+	var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+	var analyser = audioCtx.createAnalyser();
 
-container.addEventListener('click', function(){
-    const audio1 = document.getElementById('audio1');
+	var file = 'https://iondrimbafilho.me/ready_player_one.mp3';
+	var playing = false;
 
-const audioContext = new AudioContext();
-    audio1.play();
-    audioSource = audioContext.createMediaElementSource(audio1);
-    analyser = audioContext.createAnalyser();
-    audioSource.connect(analyser);
-    analyser.connect(audioContext.destination);
-    analyser.fftSize = 128;
-    const bufferLength = analyser.frequencyBinCount;
-    const dataArray = new Uint8Array(bufferLength);
+	audioElement.volume = .5;
 
-    const barWidth = 15;
-    let barHeight;
-    let x;
+	var source = audioCtx.createMediaElementSource(audioElement);
 
-    function animate(){
-        x = 0;
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        analyser.getByteFrequencyData(dataArray);
-        drawVisualiser(bufferLength, x, barWidth, barHeight, dataArray);
-        requestAnimationFrame(animate);
-    }
-    animate();
-});
+	source.connect(analyser);
+	source.connect(audioCtx.destination);
 
-file.addEventListener('change', function(){
-    const files = this.files;
-    const audio1 = document.getElementById('audio1');
-    audio1.src = URL.createObjectURL(files[0]);
-    audio1.load();
-    audio1.play();
+	var bufferLength = analyser.frequencyBinCount;
+	var frequencyData = new Uint8Array(bufferLength);
 
-    audioSource = audioContext.createMediaElementSource(audio1);
-    analyser = audioContext.createAnalyser();
-    audioSource.connect(analyser);
-    analyser.connect(audioContext.destination);
-    analyser.fftSize = 128;
-    const bufferLength = analyser.frequencyBinCount;
-    const dataArray = new Uint8Array(bufferLength);
+	analyser.smoothingTimeConstant = .5;
 
-    const barWidth = 15;
-    let barHeight;
-    let x;
+	var canvas = document.getElementById('wave');
 
-    function animate(){
-        x = 0;
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        analyser.getByteFrequencyData(dataArray);
-        drawVisualiser(bufferLength, x, barWidth, barHeight, dataArray);
-        requestAnimationFrame(animate);
-    }
-    animate();
-});
+	drawCanvasWidth();
+	drawCanvasHeight();
 
-function drawVisualiser(bufferLength, x, barWidth, barHeight, dataArray){
-    for (let i = 0; i < bufferLength; i++){
-        barHeight = dataArray[i] * 1.2;
-        ctx.save();
-        ctx.translate(canvas.width/2, canvas.height/2);
-        ctx.rotate(i * Math.PI * 4/ bufferLength);
-        const hue = 240 + i * 1.5;
-        ctx.lineWidth = barHeight/10;
-        ctx.strokeStyle = 'hsl(' + hue + ',100%, 50%)';
-        ctx.fillStyle = 'hsl(' + hue + ',100%, 50%)';;
-        ctx.beginPath();
-        ctx.moveTo(0,0);
-        ctx.lineTo(0, barHeight);
-        ctx.stroke();
-        ctx.beginPath();
-        ctx.arc(0, barHeight + barHeight/5 , barHeight/20, 0, Math.PI * 2);
-        ctx.fill();
-        ctx.beginPath();
-        ctx.arc(0, barHeight + barHeight/2, barHeight/10, 0, Math.PI * 2);
-        ctx.fill();
-        ctx.restore();
-    }
+	var ctx = canvas.getContext('2d');
+
+	ctx.lineWidth = 10;
+
+	audioElement.addEventListener('playing', function () {
+		playing = true;
+	});
+
+	audioElement.addEventListener('pause', function () {
+		playing = false;
+	});
+
+	audioElement.addEventListener('ended', function () {
+		playing = false;
+	});
+
+	window.onresize = function () {
+		canvasWidth = document.body.clientWidth;
+		drawCanvasWidth();
+	};
+
+	this.loadSound = function () {
+		var request = new XMLHttpRequest();
+
+		request.open('GET', file, true);
+
+		request.onload = function () {
+			audioElement.src = file;
+			audioElement.load();
+
+			document.querySelector('.btn-play').addEventListener('click', function () {
+				audioCtx.resume();
+				audioElement.play();
+			});
+
+			document.querySelector('.btn-pause').addEventListener('click', function () {
+				audioElement.pause();
+			});
+		}
+		request.send();
+	}
+
+	function drawCanvasWidth() {
+		canvas.style.width = (document.getElementsByTagName('section')[0].getBoundingClientRect().width) + 'px';
+		canvas.width = parseInt(canvas.style.width);
+	}
+
+	function drawCanvasHeight() {
+		canvas.style.height = '300px';
+		canvas.height = parseInt(canvas.style.height);
+	}
+
+	function drawBar(left, top, width, height, color) {
+		ctx.fillStyle = color;
+		ctx.beginPath();
+		ctx.arc(left, height, 10, 0, 6 * Math.PI);
+		ctx.fillStyle = color;
+		ctx.fill();
+	}
+
+	function play() {
+		audioElement.resume();
+		audioElement.play();
+	}
+
+	function drawWave() {
+		if (playing) {
+			analyser.getByteFrequencyData(frequencyData)
+
+			ctx.clearRect(0, 0, canvas.width, canvas.height);
+			var width = 10;
+
+			for (var j = 0; j < canvas.width; j++) {
+				var left = (width + 10) * j;
+				var topByte = (frequencyData[j]);
+				var y = (topByte);
+
+				if ((y > 10) && (y < 30)) {
+					drawBar(left, y + y, width, y + 26, '#F7F1DE');
+				}
+			}
+		}
+
+		setTimeout(function () {
+			requestAnimationFrame(drawWave);
+		}, 30);
+	}
+
+	drawWave();
 }
-  
+
+document.addEventListener('DOMContentLoaded', function () {
+	window.app = new App();
+	app.loadSound();
+
+})
