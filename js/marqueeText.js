798e587045cd85a286406ca77cfb264756a4624b
Jeareth Venegas <venegasjea@gmail.com>
2021-07-22 20:15:02 -0600

    arreglos del marquee text

--- js/marqueeText.js
+++ js/marqueeText.js
@@ -1,16 +1,17 @@
+/* eslint-disable no-param-reassign */
 const text = document.getElementById('marquee__text');
-
-slide(text);
 
 function slide(element) {
   // offsetWidth devuelve el ancho del layout del elemento y es una propiedad de solo lectura.
   const elementWidth = element.offsetWidth;
   const parentWidth = element.parentElement.offsetWidth;
-  let flag = 0;
+  let i = 0;
   setInterval(() => {
-    element.style.marginLeft = --flag + "px";
-    if (elementWidth === -flag) {
-      flag = parentWidth;
+    element.style.marginLeft = `${--i}px`;
+    if (elementWidth === -i) {
+      i = parentWidth;
     }
   }, 10);
 }
+
+slide(text);
