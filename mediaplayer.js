17cb4717f11cb7c32160324a92535793ff4846f8
mariaconejo <mary03081@gmail.com>
2021-08-04 18:17:17 -0600

    se corrigio observador y canvas y se agrego resume

--- mediaplayer.js
+++ mediaplayer.js
@@ -1,9 +1,9 @@
 import { audio, stopBtn, startBtn } from './js/util.js';
 import Canvas from './js/canvas.js';
-import Observer from './js/Observer.js';
+import ObserverMediaPlayer from './js/Observer.js';
 
-const startObv = new Observer(startBtn);
-const stopObv = new Observer(stopBtn);
+const startObv = new ObserverMediaPlayer(startBtn, 'click');
+const stopObv = new ObserverMediaPlayer(stopBtn, 'click');
 const canvas = new Canvas();
 const audioCtx = new AudioContext();
 const audioSrc = audioCtx.createMediaElementSource(audio);
@@ -12,19 +12,20 @@ const bufferLength = audioAnalyser.frequencyBinCount;
 const dataArray = new Uint8Array(bufferLength);
 
 function animateCanvas() {
+  audioCtx.resume();
   audioSrc.connect(audioAnalyser);
   audioAnalyser.connect(audioCtx.destination);
   audioAnalyser.fftSize = 1024;
   audioAnalyser.smoothingTimeConstant = 0;
   audioAnalyser.getByteTimeDomainData(dataArray);
   canvas.clearCanvas();
-  for (let i = 0; i < bufferLength; i++) {
-    canvas.drawCanvas(i, dataArray);
-    console.log(dataArray);
-  }
-  if (audio.play) {
-    window.requestAnimationFrame(animateCanvas);
-  }
+
+  canvas.drawCanvas(dataArray, bufferLength);
+  // console.log(dataArray);
+
+  // if (audio.play) {
+  window.requestAnimationFrame(animateCanvas);
+  // }
 }
 
 function mediaplayerPlay() {
@@ -49,11 +50,3 @@ function mediaplayerPause() {
 
 startObv.addObserver(mediaplayerPlay);
 stopObv.addObserver(mediaplayerPause);
-
-startObv.button.addEventListener('click', () => {
-  startObv.notifyObservers();
-});
-
-stopObv.button.addEventListener('click', () => {
-  stopObv.notifyObservers();
-});
